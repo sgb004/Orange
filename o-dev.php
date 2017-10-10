c53b2b708ea0acfcedc8896b7a494bf58530e0cc
sgb004 <sgb004@gmail.com>
2017-10-09 20:43:47 -0500

    Update

--- o-dev.php
+++ o-dev.php
@@ -1,1 +1,17 @@
+<?php
+if( isset( $argv[1] ) ){
+	$path = __DIR__;
+	if($path === '__DIR__'){
+		$path = getcwd();
+	}
 
+	define('ABSPATH', $path.'/');
+
+	require_once ABSPATH.'o-config.php';
+	
+	switch ( $argv[1] ){
+		case 'update':
+			require_once ABSPATH.'o_dev/update.php';
+	}
+}
+?>
