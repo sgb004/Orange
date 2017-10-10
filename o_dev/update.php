a4540f4165da92ae69da51e41464efcb78228130
sgb004 <sgb004@gmail.com>
2017-10-09 20:50:40 -0500

    Summary

---
+++ o_dev/update.php
@@ -0,0 +1,18 @@
+<?php
+echo 'Actualizando rutas.'."\r\n";
+$routesFull = '<?php $routes = array(';
+$MODULES = array_reverse( $MODULES );
+foreach ( $MODULES as $moduleName => $attrs ) {
+	$routes = ABSPATH.O_SRCS.$moduleName.'/routing.php';
+	if( file_exists( $routes ) ){
+		require_once $routes;
+
+		foreach ($routes as $routeKey => $routeAttrs) {
+			$routesFull .= "'".$routeKey."' => array( 'paths' => array( '".implode( "', '", $routeAttrs['paths'] )."' ), 'm' => '".$routeAttrs['m']."', 'c' => '".$routeAttrs['c']."', 'v' => '".$routeAttrs['v']."' ), ";
+		}
+	}
+}
+$routesFull .= '); ?>';
+file_put_contents( ABSPATH.'o-routing.php' , $routesFull );
+echo 'Rutas actualizadas.'."\r\n";
+?>
